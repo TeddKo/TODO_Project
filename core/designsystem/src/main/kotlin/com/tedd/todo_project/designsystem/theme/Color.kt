09f496446892241560215896524c92703407eb9a
Tedd <tddy.ko@kakao.com>
2025-07-14 17:03:26 +0900

    feat: 아이콘 추가

--- core/designsystem/src/main/kotlin/com/tedd/todo_project/designsystem/theme/Color.kt
+++ core/designsystem/src/main/kotlin/com/tedd/todo_project/designsystem/theme/Color.kt
@@ -8,5 +8,4 @@ val PrimaryColor = Color(0xFF232FA4)
 val SecondaryColor = Color(0xFFE6E8F6)
 val BorderColor = Color(0xFFD8D8D8)
 val Positive = Color(0xFF05CE62)
-
 val WarningColor = Color(0xFFFF0000)
