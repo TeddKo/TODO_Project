ced9890572712b122e4400e176644ceb469f48f7
Tedd <tddy.ko@kakao.com>
2025-07-15 05:33:03 +0900

    feat: 리뷰반영

--- core/ui/src/main/kotlin/com/tedd/todo_project/ui/extensions/Modifier.kt
+++ core/ui/src/main/kotlin/com/tedd/todo_project/ui/extensions/Modifier.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.input.pointer.PointerEventPass
+import androidx.compose.ui.input.pointer.PointerInputChange
+import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
 import kotlinx.coroutines.coroutineScope
 
@@ -17,25 +19,41 @@ fun Modifier.addFocusCleaner(focusManager: FocusManager) = this.pointerInput(Uni
     )
 }
 
-fun Modifier.disableSplitMotionEvents() =
-    this.pointerInput(Unit) {
-        coroutineScope {
-            var currentId: Long = -1L
-            awaitPointerEventScope {
-                while (true) {
-                    awaitPointerEvent(PointerEventPass.Initial).changes.forEach { pointerInfo ->
-                        when {
-                            pointerInfo.pressed && currentId == -1L -> currentId =
-                                pointerInfo.id.value
+fun Modifier.disableSplitMotionEvents() = this.pointerInput(Unit) {
+    handlePointerEvents()
+}
 
-                            pointerInfo.pressed.not() && currentId == pointerInfo.id.value -> currentId =
-                                -1
+private suspend fun PointerInputScope.handlePointerEvents() {
+    coroutineScope {
+        val pointerTracker = PointerTracker()
 
-                            pointerInfo.id.value != currentId && currentId != -1L -> pointerInfo.consume()
-                            else -> Unit
-                        }
-                    }
+        awaitPointerEventScope {
+            while (true) {
+                val event = awaitPointerEvent(PointerEventPass.Initial)
+
+                pointerTracker.processPointerChanges(event.changes)
+            }
+        }
+    }
+}
+
+private class PointerTracker {
+    private var currentId: Long = -1L
+    fun processPointerChanges(changes: List<PointerInputChange>) {
+        changes.forEach { pointerInfo ->
+            when {
+                pointerInfo.pressed && currentId == -1L -> {
+                    currentId = pointerInfo.id.value
+                }
+
+                !pointerInfo.pressed && currentId == pointerInfo.id.value -> {
+                    currentId = -1L
+                }
+
+                pointerInfo.id.value != currentId && currentId != -1L -> {
+                    pointerInfo.consume()
                 }
             }
         }
     }
+}
